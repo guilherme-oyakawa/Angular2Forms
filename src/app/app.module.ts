c1882e2a7042c0cccdf482de8a8876d69ddc3adb
OPUS-Guilherme.Masao <guilherme.masao@opus-software.com.br>
2017-03-06 15:04:19 -0300

    Formulário de agendamento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { SelectModule } from 'ng2-select';
 
 import { AppComponent } from './app.component';
+import { HeroFormComponent } from './hero-form.component';
+import { AutocompleteComponent} from './autocomplete.component';
+import { SelectMultipleComponent} from './select-multiple.component';
+import { SelectDuplaComponent} from './select-dupla.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroFormComponent,
+    SelectMultipleComponent,
+    SelectDuplaComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    SelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
